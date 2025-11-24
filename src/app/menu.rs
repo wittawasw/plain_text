dfdd1fd49e9c20a87014b392ad4d93a50e70517a
wittawasw <start@wittawasw.com>
2025-11-24 09:33:50 +0700

    save existing file, new file

--- src/app/menu.rs
+++ src/app/menu.rs
@@ -23,7 +23,6 @@ where
         let buf = Rc::clone(buf);
         let stylebuf = Rc::clone(stylebuf);
         let state = Rc::clone(state);
-
         let update_status_open = update_status.clone();
 
         menu.add("File/Open\t", Shortcut::Ctrl | 'o', MenuFlag::Normal, move |_| {
@@ -42,16 +41,53 @@ where
     {
         let buf = Rc::clone(buf);
         let state = Rc::clone(state);
-
         let update_status_save = update_status.clone();
 
-        menu.add("File/Save As\t", Shortcut::Ctrl | 's', MenuFlag::Normal, move |_| {
-            if let Some(path) = dialog::file_chooser("Save File", "*", ".", true) {
+        menu.add("File/Save\t", Shortcut::Ctrl | 's', MenuFlag::Normal, move |_| {
+            let current_path = state.borrow().filepath.clone();
+
+            // If no existing file, fallback to Save As
+            let path = if current_path.is_empty() {
+                dialog::file_chooser("Save File", "*", ".", true)
+            } else {
+                Some(current_path)
+            };
+
+            if let Some(path) = path {
                 let text = buf.borrow().text();
                 let _ = fs::write(&path, text);
                 state.borrow_mut().filepath = path.clone();
                 update_status_save();
             }
+        });
+    }
+
+    {
+        let buf = Rc::clone(buf);
+        let state = Rc::clone(state);
+        let update_status_saveas = update_status.clone();
+
+        menu.add("File/Save As", Shortcut::None, MenuFlag::Normal, move |_| {
+            if let Some(path) = dialog::file_chooser("Save File", "*", ".", true) {
+                let text = buf.borrow().text();
+                let _ = fs::write(&path, text);
+                state.borrow_mut().filepath = path.clone();
+                update_status_saveas();
+            }
+        });
+    }
+
+    {
+        let buf = Rc::clone(buf);
+        let stylebuf = Rc::clone(stylebuf);
+        let state = Rc::clone(state);
+        let update_status_new = update_status.clone();
+
+        menu.add("File/New\t", Shortcut::Ctrl | 'n', MenuFlag::Normal, move |_| {
+            buf.borrow_mut().set_text("");
+            stylebuf.borrow_mut().set_text("A");
+            state.borrow_mut().filepath.clear(); // mark as new file
+            update_status_new();
         });
     }
 
